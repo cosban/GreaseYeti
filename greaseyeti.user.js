769b53f95267aaf106e7ada8278234696ed83f81
headbanger <buzbee.p@gmail.com>
2016-10-06 13:48:37 -0700

    Use single each() loop for highlighting messages
    Fixes a bug where [Like] / [Repeat] only appeared on the first post. We
    were calling `top.html()` when top didn't necessarily exist. We should
    only need a single .each() loop here -- we're looking at all message-top
    instances.

--- greaseyeti.user.js
+++ greaseyeti.user.js
@@ -1377,39 +1377,30 @@ function stealthLogout() {
 }
 
 function highlightPost(message_container) {
-    message_container.children('.message-top').each(function () {
-        var message_top = [];
-        message_top.push($(this));
-        var quotes = $(this).parent().find('.message-body .message-top');
-        if (quotes) {
-            for (var i = 0; i < quotes.length; i++) {
-                var child = quotes[i];
-                message_top.push($(child));
+    // The first message is the actual message header. If there are
+    // other .message-tops, they are quotes.
+    message_container.find('.message-top').each(function() {
+        var poster = findWhoPostedMessage($(this).html());
+        if (!anon_topic) {
+            // Make sure it's not an anon topic
+            if (poster.indexOf('#') != -1) {
+                anon_topic = true;
+             } else {
+                var hcolor = getHighlightColor(poster, true);
+                if (hcolor !== false) {
+                    $(this).css('background-color', hcolor);
+                    $(this).parent().css('border-color', hcolor);
+                }
             }
         }
-        for (var index in message_top) {
-            var top = message_top[index];
-            var poster = findWhoPostedMessage(top.html());
-            if (!anon_topic) {
-                // Make sure it's not an anon topic
-                if (poster.indexOf('#') != -1) {
-                    anon_topic = true;
-                } else {
-                    var hcolor = getHighlightColor(poster, true);
-                    if (hcolor !== false) {
-                        top.css('background-color', hcolor);
-                        top.parent().css('border-color', hcolor);
-                    }
-                }
+        if (anon_topic) {
+            findMyHumanNumber();
+            if (my_human_number == 0 || !ch('my_anon')) {
+                return;
             }
-            if (anon_topic) {
-                findMyHumanNumber();
-                if (my_human_number == 0 || !ch('my_anon')) {
-                    return;
-                }
-                if ('Human #' + my_human_number == poster) {
-                    top.css('background-color', ch('my_color'));
-                }
+            if ('Human #' + my_human_number == poster) {
+                $(this).css('background-color', ch('my_color'));
+                $(this).parent().css('border-color', ch('my_color'));
             }
         }
     });
